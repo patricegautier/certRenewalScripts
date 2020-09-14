30500e2ba9f3b0dab3fecebdc1347f0c41f7b87e
Patrice Gautier <patrice@mac.com>
2020-09-13 22:58:22 -0700

    Allow for sshpass to not be present

--- updatePublicKey.sh
+++ updatePublicKey.sh
@@ -4,7 +4,7 @@
 usage()
 {
 	echo "Usage "${0}" [-i privateKeyPath] [-s <fileName>] user@targetMachine"
-	echo "-s use sshpass with given password file"
+	echo "-s use sshpass with given password file. Will ask for the password if sshpass is not installed"
     echo "  returns "
 	exit 2
 }
@@ -27,19 +27,19 @@ shift $((OPTIND-1))
 TARGET=$@
 
 
-if [ -z ${TARGET} ]; then
+if [[ -z ${TARGET} ]]; then
 	usage
 fi
 
 
-if [ -z ${PRIVKEY_PATH} ]; then
+if [[ -z ${PRIVKEY_PATH} ]]; then
    	PRIVKEY_PATH=${HOME}"/.ssh/id_rsa"
 fi
 
 PUBKEY_PATH=${PRIVKEY_PATH}".pub"
 PUBKEY=$(<${PUBKEY_PATH}) || exit 1;
 
-if [ -z "${PUBKEY}" ]; then
+if [[ -z "${PUBKEY}" ]]; then
 	echo "Could not read public key at "${PUBKEY_PATH}
 	exit 1
 fi
@@ -48,13 +48,14 @@ fi
 
 ssh -i ${PRIVKEY_PATH} -q -o "BatchMode yes" ${TARGET} true
 PUBKEY_OK=$?
+SSHPASS=`which sshpass`
 
 if [ ${PUBKEY_OK} != '0'  ]; then
 	echo "Need to update public key for " ${TARGET}
-	if [[ -z ${SSH_PASS_FILE} ]]; then
+	if [[ -z ${SSH_PASS_FILE} ]] || [[ -z ${SSHPASS} ]]; then
 	   	ssh -i ${PRIVKEY_PATH}  ${TARGET} "mkdir -p .ssh && echo '${PUBKEY}' >> .ssh/authorized_keys" || exit 1;
 	else
-	   	sshpass -f ${SSH_PASS_FILE} ssh -o "BatchMode yes" -i ${PRIVKEY_PATH}  ${TARGET} "mkdir -p .ssh && echo '${PUBKEY}' >> .ssh/authorized_keys" || exit 1;		
+	   	sshpass -f ${SSH_PASS_FILE} ssh -o -i ${PRIVKEY_PATH}  ${TARGET} "mkdir -p .ssh && echo '${PUBKEY}' >> .ssh/authorized_keys" || exit 1;		
 	fi
 fi
 
