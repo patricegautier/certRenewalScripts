c323e9e68882b8c5377f249871cfbe17b0394ffe
Patrice Gautier <patrice@mac.com>
2021-01-13 20:22:10 -0800

    Added extra processing for dropbear on protect cameras

--- updatePublicKey.sh
+++ updatePublicKey.sh
@@ -6,20 +6,23 @@ usage()
 	echo "Usage "${0}" [-d] [-i privateKeyPath] [-s <fileName>] user@targetMachine"
 	echo "-i specify private public key pair"
 	echo "-s use sshpass with given password file. Will ask for the password if sshpass is not installed"
-	echo "-d disabled strick host key checking with SSH option StrictHostKeyChecking=no"	
+	echo "-d disabled strict host key checking with SSH option StrictHostKeyChecking=no"
+	echo "-b for dropbear (used in protect cameras) additionally copy ~/.ssh/authorized_keys to /var/etc/dropbear"
 	exit 2
 }
 
 
 
 STRICT=""
+unset DROPBEAR
 
-while getopts 'i:s:d' OPT
+while getopts 'i:s:db' OPT
 do
   case $OPT in
     i) PRIVKEY_PATH=${OPTARG} ;;
     s) SSH_PASS_FILE=${OPTARG} ;;
     d) STRICT="-o StrictHostKeyChecking=no" ;;
+	b) DROPBEAR=true ;;
   esac
 done
 
@@ -56,6 +59,9 @@ if [ ${PUBKEY_OK} != '0'  ]; then
 	echo Need to update public key for ${TARGET}
 	if [[ -z ${SSH_PASS_FILE} ]] || ! [[ -e ${SSH_PASS_FILE} ]] || [[ -z ${SSHPASS} ]]; then
 	   	ssh ${STRICT} ${TARGET} "mkdir -p .ssh && echo '${PUBKEY}' >> .ssh/authorized_keys" || exit 1;
+	   	if ! [[ -z ${DROPBEAR} ]]; then
+		   	ssh ${TARGET} "cp .ssh/authorized_keys /var/etc/dropbear/" || exit 1;	   	
+	   	fi
 	else
 	   	sshpass -f ${SSH_PASS_FILE} ssh ${STRICT} ${TARGET} "mkdir -p .ssh && echo '${PUBKEY}' >> .ssh/authorized_keys" || exit 1;		
 	fi
